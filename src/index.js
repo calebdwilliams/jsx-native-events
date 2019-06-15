b5ffb87d417c5ba065c30092acfcf2833811e7da
Caleb Williams <caleb.d.williams@gmail.com>
2019-06-15 13:14:14 -0500

    chore: Initial commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,41 @@
-import React, {Component} from 'react'
+import React from 'react'
 
-export default class extends Component {
-  render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
+/**
+ * Convert a string from camelCase to kebab-case
+ * @param {string} string - The base string (ostensibly camelCase)
+ * @return { string } - A kebab-case string
+ */
+const toKebabCase = string => string.replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, '$1-$2').toLowerCase()
+const listeners = Symbol('event listeners')
+
+export function ink (type, props, ...children) {
+  const newProps = { ...props }
+  newProps.ref = (element) => {
+    if (element) {
+      if (props) {
+        const keys = Object.keys(props)
+        keys
+          .filter(key => key.match(/^onEvent/))
+          .map(key =>
+            ({
+              key,
+              eventName: toKebabCase(
+                key.replace('onEvent', '')
+              ).replace('-', '')
+            })
+          )
+          .map(({ eventName, key }) => {
+            if (!element[listeners]) {
+              element[listeners] = new Map()
+            }
+            if (!element[listeners].has(eventName)) {
+              element.addEventListener(eventName, props[key])
+              element[listeners].set(eventName, props[key])
+            }
+          })
+      }
+    }
   }
+
+  return React.createElement.apply(null, [type, newProps, ...children])
 }
