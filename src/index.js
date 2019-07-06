13e54fc2136a9fe38871a5c7c37f198e6593799c
Gus Garzaki <gusgarzaki@gmail.com>
2019-07-06 00:49:39 -0500

    add behaviour for properties

--- src/index.js
+++ src/index.js
@@ -14,39 +14,44 @@ const eventPattern = /^onEvent/
 
 export default function jsx (type, props, ...children) {
   const newProps = { ...props }
+
   if (typeof type === 'string') {
     newProps.ref = (element) => {
       if (element) {
         if (props) {
-          const keys = Object.keys(props)
-          /** Get all keys that have the `onEvent` prefix */
-          keys
-            .filter(key => key.match(/^onEvent/))
-            .map(key =>
-              ({
-                key,
-                eventName: toKebabCase(
-                  key.replace('onEvent', '')
-                ).replace('-', '')
-              })
-            )
-            .map(({ eventName, key }) => {
-              /** Add the listeners Map if not present */
-              if (!element[listeners]) {
-                element[listeners] = new Map()
-              }
-  
-              /** If the listener hasn't be attached, attach it */
-              if (!element[listeners].has(eventName)) {
-                element.addEventListener(eventName, props[key])
-                /** Save a reference to avoid listening to the same value twice */
-                element[listeners].set(eventName, props[key])
-              }
-            })
+          /** Map custom events as objects (must have onEvent prefix) */
+          const events = Object.entries(props).filter(([k, v]) => k.match(eventPattern)).map(([k, v]) => ({ [k]: v }))
+          /** Get only the complex props (objects and arrays) */
+          const complexProps = Object.entries(props).filter(([k, v]) => typeof v === 'object').map(([k, v]) => ({ [k]: v }))
+
+          // console.log(events, complexProps)
+
+          for (const event of events) {
+            const [key, impl] = Object.entries(event)[0]
+            const eventName = toKebabCase(
+              key.replace('onEvent', '')
+            ).replace('-', '')
+
+            /** Add the listeners Map if not present */
+            if (!element[listeners]) {
+              element[listeners] = new Map()
+            }
+            /** If the listener hasn't be attached, attach it */
+            if (!element[listeners].has(eventName)) {
+              element.addEventListener(eventName, impl)
+              /** Save a reference to avoid listening to the same value twice */
+              element[listeners].set(eventName, impl)
+            }
+          }
+
+          for (const prop of complexProps) {
+            const [key, value] = Object.entries(prop)[0]
+            delete newProps[key] // remove the complex prop from props
+            element[key] = value // assign the complex prop as property instead attribute
+          }
         }
       }
+    }
   }
-  }
-  
   return React.createElement.apply(null, [type, newProps, ...children])
 }
